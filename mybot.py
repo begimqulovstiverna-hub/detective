b90000c4ace3f170acfee4ec8b74d94aa0f39038
begimqulovstiverna-hub <begimqulovstiverna@gmail.com>
2026-02-15 12:53:31 +0500

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -7,6 +7,22 @@ import requests
 from fpdf import FPDF
 from PIL import Image
 from PIL.ExifTags import TAGS
+##############################
+from flask import Flask
+import threading
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+########################################
+# Botni alohida oqimda (thread) ishga tushirish
+t = threading.Thread(target=run)
+t.start()
 
 # ⚠️ TOKENINGIZNI KIRITING
 TOKEN = '8372622031:AAFaKQlDCKcEO0qPDPXASHTisTdyn_O_UgU'
